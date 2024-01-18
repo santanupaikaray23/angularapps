33476a5cf8f9aee8ea4ef01e9083ab46ddd57d18
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-01-19 00:08:09 +0530

    add changes

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Cart } from '../shared/models/Cart';
+import { CartItem } from '../shared/models/cartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './cart-page.component.css'
 })
 export class CartPageComponent {
+cart!: Cart;
+constructor(private cartService: CartService){
+  this.setCart();
+}
+ngOnInit(): void{
 
+}
+setCart(){
+  this.cart = this.cartService.getCart();
+}
+removeFromCart(cartItem:CartItem){
+  this.cartService.removeFromCart(cartItem.food.id);
+  this.setCart();
+}
+changeQuantity(cartItem:CartItem, quantityInString:string){
+  const quantity = parseInt(quantityInString);
+  this.cartService.changeQuantity(cartItem.food.id, quantity);
+  this.setCart();
+}
 }
--- src/app/foodpage/foodpage.component.ts
+++ src/app/foodpage/foodpage.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Foods } from '../shared/models/food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-foodpage',
@@ -12,11 +13,15 @@ export class FoodpageComponent {
   food!:Foods;
 
   constructor(private activatedRoute:ActivatedRoute,
-    private foodService:FoodService){
+    private foodService:FoodService, private cartService:CartService,
+    private router:Router){
       activatedRoute.params.subscribe((params)=>{
         if(params['id'])
         this.food = foodService.getFoodById(params['id'])
       })
     }
-
+addToCart(){
+  this.cartService.addToCart(this.food);
+  this.router.navigateByUrl('/cart-page')
+}
 }
